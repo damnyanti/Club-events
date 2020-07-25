e9533404a7e0ae55261d6a91f6c5372223f2a810
priyaagulati <priyagulatipg12@gmail.com>
2020-07-25 13:23:42 +0530

    created new app club and its models

---
+++ club/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+CLUBS=[
+    ('Coding Club','Coding Club IITG'),
+    ('Electronics','Electroncis Club IITG'),
+    ('Robotics','Robotics Club IITG'),
+    ('LitSoc','Literary Society IITG'),
+    ('Xpressions','Drama Club IITG'),
+    ('Aeromodelling','Aeromodelling Club IITG'),
+    ('Cadence','Dance Club IITG'),
+    ('IITG.AI','Artificial intelligence Club IITG'),
+]
+
+class Club(models.Model):
+        club_name = models.CharField(max_length=50, choices=CLUBS)
+        secretary = models.CharField(max_length=30)
+
+        def __str__(self):
+            return self.club_name
+
+class Post(models.Model):
+    club_name = models.ForeignKey(Musician, on_delete=models.CASCADE)
+    uid = models.CharField(max_length=30)
+    updated_on  = models.CharField(max_length=50)
+    content     = models.TextField()
+
+    def __str__(self):
+        return self.content
--- clubevents/urls.py
+++ clubevents/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from events import views as events_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('club/', include('club.urls')),
     path('', events_views.home, name='home'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html',redirect_authenticated_user=False), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
