40daf5600866c9fecf531fb0864fee59a8a1cb4a
priyaagulati <49652510+priyaagulati@users.noreply.github.com>
2020-07-26 21:34:31 +0530

    Update urls.py

--- club/urls.py
+++ club/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
 
-app_name='club'
-
 urlpatterns = [
     path('', views.index, name='index'),
     path('<club_id>/', views.detail, name='detail'),
